a92d2d27a7d2a93e2140b7e176c925c8da31a9ba
Joshua Biong <joshuabiong7@gmail.com>
2023-12-15 13:23:29 +0800

    starting to create a projectile from the player

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ canvas.height = innerHeight
 
 const cntx = canvas.getContext('2d');
 
+
+// # classess
+// Plaaaaayer
 class Player {
     constructor(x, y, radius, color) {
         this.x = x
@@ -20,6 +23,7 @@ class Player {
     }
 }
 
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
@@ -27,4 +31,32 @@ const player = new Player(x, y, 30, this.color)
 player.drawn()
 
 
-console.log(player)
+// Projectile from the Player
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    drawn() {
+        cntx.beginPath()
+        cntx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        cntx.fillStyle = 'blue'
+        cntx.fill()
+    }
+
+}
+
+
+
+// console.log(player)
+
+addEventListener('click', (event) => {
+    // console.log(event.clientX)
+    console.log(event)
+    const projectile = new Projectile(event.clientX, event.clientY, 7, this.color, null)
+    projectile.drawn()
+
+})
